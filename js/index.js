f0c3826ce59398799a41b183916a838129c14b10
Ezequiel Elizalde <elizaldeezequiel@gmail.com>
2024-06-18 09:52:25 -0300

    V1.1

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ function calcularCuota() {
     if (deuda <= 2 || cantidadCuotas <= 2) {
         alert("Los valores de deuda y cantidad de cuotas deben ser mayores o iguales a 2");
     }
-    else (deuda > 2 || cantidadCuotas > 2) {
+    else (deuda > 2 || cantidadCuotas > 2); {
     let montoCuota = deuda / cantidadCuotas;
     document.getElementById("montoCuota").value = montoCuota.toFixed(2);
     console.log(`La cuota mensual es de ${montoCuota.toFixed(2)}`)
